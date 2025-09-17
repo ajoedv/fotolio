9670178430ca649ad5e7836029f098fcd217cc2e
jodev <mamered@gmail.com>
2025-09-18 01:13:06 +0200

    Update settings.py for Heroku deployment

--- fotolio/settings.py
+++ fotolio/settings.py
@@ -33,7 +33,7 @@ DEBUG = os.environ.get("DEBUG", "False") == "True"
 ALLOWED_HOSTS = [
     "localhost",
     "127.0.0.1",
-    "yourappname.herokuapp.com",
+    "fotolio.herokuapp.com",
 ]
 
 # Application definition
