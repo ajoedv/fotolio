e967388ea348343f9b0198a2568d036802f62154
jodev <mamered@gmail.com>
2025-09-17 04:30:44 +0200

    Add product detail page with base size info and custom styling

---
+++ products/constants.py
@@ -0,0 +1,7 @@
+BASE_SIZE_LABEL = "12 x 18 in (unframed)"
+
+AVAILABLE_SIZES = [
+    {"label": "12 x 18 in (unframed)", "price": 89.00},
+    {"label": "16 x 24 in (unframed)", "price": 129.00},
+    {"label": "20 x 30 in (unframed)", "price": 179.00},
+]
--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Product
+from .constants import BASE_SIZE_LABEL  # ← جديد
 
 def ping(request):
     return HttpResponse("Products route OK")
@@ -8,3 +9,11 @@ def ping(request):
 def products_list(request):
     products = Product.objects.select_related('category').order_by('name')
     return render(request, 'products/list.html', {'products': products})
+
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    context = {
+        "product": product,
+        "base_size": BASE_SIZE_LABEL,
+    }
+    return render(request, "products/detail.html", context)
