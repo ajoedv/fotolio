780ab8d779b5dcf7e511bfba9fdda8e9f8e97cee
jodev <mamered@gmail.com>
2026-03-11 21:46:38 +0100

    Fix newsletter form validation and frontend notification messages

--- newsletter/forms.py
+++ newsletter/forms.py
@@ -1,5 +1,8 @@
 from django import forms
+from .models import NewsletterSubscriber
 
 
-class NewsletterSubscribeForm(forms.Form):
-    email = forms.EmailField()
+class NewsletterSubscribeForm(forms.ModelForm):
+    class Meta:
+        model = NewsletterSubscriber
+        fields = ['email']
